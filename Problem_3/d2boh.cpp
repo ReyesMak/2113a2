b2c8575bd1c182d2348fcf8b6a8c89de45bcc765
Reyes Mak <108719212+ReyesMak@users.noreply.github.com>
2022-11-03 13:53:00 +0800

    Comments add for d2boh.cpp

--- Problem_3/d2boh.cpp
+++ Problem_3/d2boh.cpp
@@ -1,5 +1,5 @@
 // d2boh.cpp
-
+//Include the necessary libraries
 #include <iostream>
 #include <cmath>
 #include "d2boh.h"
@@ -11,13 +11,14 @@ int decimal_to_binary(int input, int output[10]){
 	//Display digits from higher index to lower index
 	for (int i = 9; i >= 0 ; --i){
 		if (input / pow(2,i) >= 1){
-			//The digit representing the {i}th exponents of 2 is stored at the (9-i)th index of the array {output}
+			//The digit representing the {i} power of 2 is stored at the (9-i)th index of the array {output}
 			output[9-i]=floor(input/pow(2,i));
-			//{input} reduces by 2^i after calculating the digit
+			//{input} decreases by the decimal value of the calculated digit
 			input-=pow(2,i);
 			//{count} increments by 1 after calculating the digit
 			count++;
 		}
+		//Ensure {count} increments by 1 if the calculated digit is equal to 0
 		else if (count > 0){
 			count++;
 		}
@@ -31,10 +32,14 @@ int decimal_to_octal(int input, int output[10]) {
 	//Display digits from higher index to lower index
 	for (int i = 9; i >= 0 ; --i){
 		if (input / pow(8,i) >= 1){
+			//The digit representing the {i} power of 8 is stored at the (9-i)th index of the array {output}
 			output[9-i]=floor(input/pow(8,i));
+			//{input} decreases by the decimal value of the calculated digit
 			input-=pow(8,i)*floor(input/pow(8,i));
+			//{count} increments by 1 after calculating the digit
 			count++;
 		}
+		//Ensure {count} increments by 1 if the calculated digit is equal to 0
 		else if (count > 0){
 			count++;
 		}
@@ -47,18 +52,26 @@ int decimal_to_hexadecimal(int input, char output[10]) {
 	int count = 0;
 	for (int i = 9; i >= 0 ; --i){
 		if (input / pow(16,i) >= 1){
-			// consider ascii value
+			//Below is based on the ASCII decimal value of characters
 			if (floor(input/pow(16,i)) < 10){
+				//An addition of 48 is necessary to convert an integer digit which is smaller then 10 
+				//to a character of number digits in the array
 				output[9-i]=char(floor(input/pow(16,i)) + 48);
 			}
 			else{
+				//An addition of 55 is necessary to convert the digit which is larger than or equal to 10
+				//to a character of letters in the array
 				output[9-i]=char(floor(input/pow(16,i)) + 55);
 			}
+			//{input} decreases by the decimal value of the calculated digit
 			input-=pow(16,i)*floor(input/pow(16,i));
+			//{count} increments by 1 after calculating the digit
 			count++;
 		}
+		//Ensure {count} increments by 1 if the calculated digit is equal to 0
 		else if (count > 0){
-			count++;}
+			count++;
+		}
 	}
 	return count;
 }
